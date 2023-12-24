92d03bfd1509876860fcc0983c43b7426dfafd7a
Shubham Krishna <shubham.krishna@ml6.eu>
2023-12-24 17:58:55 +0100

    Add workshops

---
+++ workshops/beam-state-timers-quickstart/my_pipeline/pipeline.py
@@ -0,0 +1,214 @@
+#    Copyright 2023 Google LLC
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#        http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import enum
+import gc
+import json
+from turtle import st
+from typing import Optional, Iterable, List, NamedTuple
+
+import apache_beam as beam
+from apache_beam import PCollection, TimeDomain
+from apache_beam import coders
+from apache_beam.io.fileio import WriteToFiles
+from apache_beam.io.textio import ReadFromText
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.transforms.userstate import ReadModifyWriteStateSpec, BagStateSpec, TimerSpec, \
+    CombiningValueStateSpec, on_timer
+from apache_beam.transforms.window import TimestampedValue
+from apache_beam.utils.timestamp import Timestamp
+from dateutil import parser
+from requests import session
+
+
+# ---------------------------------------------------------------------------------------
+# Schema
+# ---------------------------------------------------------------------------------------
+
+# Great intro to Beam schemas in python: https://www.youtube.com/watch?v=zx4p-UNSmrA
+
+# First we create a class that inherits from NamedTuple, this is our Schema
+#
+# To actually create an instance of TaxiPoint you can leverage dictionary unpacking
+# Let's say you have a dictionary d = {"ride_id": asdf, ...,"passenger_count": 8}
+# This dictionary's keys match the fields of TaxiPoint.
+# In this case, you use dictionary unpacking '**' to make class construction easy.
+# Dictionary unpacking is when passing a dictionary to a function,
+# the key-value pairs can be unpacked into keyword arguments in a function call
+# where the dictionary keys match the parameter names of the function.
+# So the call to the constructor looks like ==> TaxiPoint(**d)
+class TaxiPoint(NamedTuple):
+    ride_id: str
+    point_idx: int
+    latitude: float
+    longitude: float
+    timestamp: str
+    meter_reading: float
+    meter_increment: float
+    ride_status: str
+    passenger_count: int
+
+
+class SessionReason(enum.Enum):
+    DROPOFF_SEEN = 1
+    GARBAGE_COLLECTION = 2
+
+
+class TaxiSession(NamedTuple):
+    ride_id: str
+    duration: float
+    start_timestamp: str
+    end_timestamp: str
+    n_points: int
+    start_status: str
+    end_status: str
+    session_reason: SessionReason
+
+
+# Second we let Beam know about our Schema by registering it
+beam.coders.registry.register_coder(TaxiPoint, beam.coders.RowCoder)
+beam.coders.registry.register_coder(TaxiSession, beam.coders.RowCoder)
+
+
+# ---------------------------------------------------------------------------------------
+# Parsing functions
+# ---------------------------------------------------------------------------------------
+
+def json_to_taxi_point(s: str) -> TaxiPoint:
+    d: dict = json.loads(s)
+    return TaxiPoint(**d)
+
+
+def add_timestamp(p: TaxiPoint) -> TaxiPoint:
+    ts: float = parser.parse(p.timestamp).timestamp()
+    return TimestampedValue(p, ts)
+
+
+def max_timestamp_combine(input_iterable):
+    return max(input_iterable, default=0)
+
+
+# --------------------------------------------------------------------------------------
+# TASK: Write a DoFn to find sessions using state & timers
+# --------------------------------------------------------------------------------------
+
+class FindSessions(beam.DoFn):
+    """This DoFn applies state & timers to try to infer the session data from the received
+    points."""
+
+    # The state for the key
+    KEY_STATE = ReadModifyWriteStateSpec('state', coders.StrUtf8Coder())
+
+    # Elements bag of taxi ride events
+    TAXI_RIDE_EVENTS_BAG = BagStateSpec('taxi_ride_events_bag',
+                                        coders.registry.get_coder(TaxiPoint))
+
+    # Event time timer for Garbage Collection
+    GC_TIMER = TimerSpec('gc_timer', TimeDomain.WATERMARK)
+
+    # The maximum element timestamp seen so far.
+    MAX_TIMESTAMP = CombiningValueStateSpec('max_timestamp_seen', max_timestamp_combine)
+
+    def process(self, element: tuple[str, TaxiPoint],
+                element_timestamp=beam.DoFn.TimestampParam,
+                key_state=beam.DoFn.StateParam(KEY_STATE),
+                taxi_ride_events_bag=beam.DoFn.StateParam(TAXI_RIDE_EVENTS_BAG),
+                max_timestamp_seen=beam.DoFn.StateParam(MAX_TIMESTAMP),
+                gc_timer=beam.DoFn.TimerParam(GC_TIMER)) -> Iterable[TaxiSession]:
+        # TODO
+        # Update the state for every new message
+        # Check if end of session is seen, and emit the session data
+        # If not, keep waiting
+        key, point = element
+        if key_state.read() is None:
+            key_state.write(key)
+        taxi_ride_events_bag.add(point)
+        max_timestamp_seen.add(element_timestamp.micros)
+        if point.ride_status == "dropoff":
+            session = self._calculate_session(key,
+                                              taxi_ride_events_bag,
+                                              SessionReason.DROPOFF_SEEN)
+            yield session
+            taxi_ride_events_bag.clear()
+            key_state.clear()
+            max_timestamp_seen.clear()
+            gc_timer.clear()
+        else:
+            # if not, keep waiting
+            max_ts = Timestamp(max_timestamp_seen.read()).seconds()
+            expiration_second: int = max_ts + 8 * 60
+            gc_timer.set(Timestamp(seconds=expiration_second))
+
+    @on_timer(GC_TIMER)
+    def expiry_callback(
+            self,
+            key_state=beam.DoFn.StateParam(KEY_STATE),
+            taxi_ride_events_bag=beam.DoFn.StateParam(TAXI_RIDE_EVENTS_BAG),
+            max_timestamp_seen=beam.DoFn.StateParam(MAX_TIMESTAMP)) -> Iterable[TaxiSession]:
+        # TODO
+        # The timer has been triggered, you need to emit a session with whatever data has been
+        # accumulated so far
+        session = self._calculate_session(key_state.read(),
+                                          taxi_ride_events_bag,
+                                          SessionReason.GARBAGE_COLLECTION)
+        yield session
+        key_state.clear()
+        taxi_ride_events_bag.clear()
+        max_timestamp_seen.clear()
+
+    @staticmethod
+    def _calculate_session(key: str,
+                           taxi_ride_events_bag: Iterable[TaxiPoint],
+                           session_reason: SessionReason) -> TaxiSession:
+        # TODO
+        # Since we emit sessions from two different methods, let's use the same function to emit
+        # sessions, so we do it with consistency.
+        points = list(taxi_ride_events_bag)
+        points.sort(key=lambda p: p.timestamp)
+
+        start_time = parser.parse(points[0].timestamp).timestamp()
+        end_time = parser.parse(points[-1].timestamp).timestamp()
+
+        s = TaxiSession(ride_id=key,
+                        duration=end_time - start_time,
+                        start_timestamp=points[0].timestamp,
+                        end_timestamp=points[-1].timestamp,
+                        n_points=len(points),
+                        start_status=points[0].ride_status,
+                        end_status=points[-1].ride_status,
+                        session_reason=session_reason)
+        return s
+
+
+@beam.ptransform_fn
+def taxi_stats_transform(json_strs: PCollection[str]) -> PCollection[TaxiSession]:
+    points: PCollection[TaxiPoint] = json_strs | "parse json strings" >> beam.Map(
+        json_to_taxi_point)
+    tstamp: PCollection[TaxiPoint] = points | "timestamping" >> beam.Map(add_timestamp)
+    key: PCollection[tuple[str, TaxiPoint]] = tstamp | "key" >> beam.WithKeys(
+        lambda e: e.ride_id)
+    sessions: PCollection[TaxiSession] = key | "stats" >> beam.ParDo(FindSessions())
+    output: PCollection[TaxiSession] = sessions | "cep" >> beam.Filter(
+        lambda s: s.duration > 3600)
+    return output
+
+
+# ------------------------------------------------------------------------------------
+# Pipeline
+# ------------------------------------------------------------------------------------
+
+def run(beam_options: Optional[PipelineOptions] = None):
+    with beam.Pipeline(options=beam_options) as pipeline:
+        rides: PCollection[str] = pipeline | "Read ndjson input" >> ReadFromText(
+            file_pattern=beam_options.input_filename)
+        calculations: PCollection[TaxiSession] = rides | "calculations" >> taxi_stats_transform()
+        writeablecalculations: PCollection[str] = calculations | beam.Map(json.dumps)
+        writeablecalculations | WriteToFiles(path=beam_options.output_filename)
---
+++ workshops/step-by-step-development-of-a-streaming-pipeline-in-Python/streamingworkshop/my_pipeline/pipeline.py
@@ -0,0 +1,249 @@
+# Copyright 2023 Google LLC
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+########################################################################################
+# Imports
+########################################################################################
+from typing import Optional
+
+import json
+import typing
+from dateutil import parser
+from datetime import datetime
+
+
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.transforms.window import Sessions
+from apache_beam.transforms.window import TimestampCombiner
+from apache_beam.transforms.trigger import AfterWatermark
+from apache_beam.transforms.trigger import AccumulationMode
+from apache_beam.utils.timestamp import Duration
+from apache_beam import PCollection
+from apache_beam.io.fileio import WriteToFiles
+from apache_beam.io.textio import ReadFromText
+from apache_beam.transforms.window import TimestampedValue
+
+########################################################################################
+# Schema
+########################################################################################
+
+# Great intro to Beam schemas in python: https://www.youtube.com/watch?v=zx4p-UNSmrA
+
+# First we create a class that inherits from NamedTuple, this is our Schema
+#
+# To actually create an instance of TaxiPoint you can leverage dictionary unpacking
+# Let's say you have a dictionary d = {"ride_id": asdf, ...,"passenger_count": 8}
+# This dictionary's keys match the fields of TaxiPoint.
+# In this case, you use dictionary unpacking '**' to make class construction easy.
+# Dictionary unpacking is when passing a dictionary to a function,
+# the key-value pairs can be unpacked into keyword arguments in a function call
+# where the dictionary keys match the parameter names of the function.
+# So the call to the constructor looks like ==> TaxiPoint(**d)
+class TaxiPoint(typing.NamedTuple):
+    ride_id: str
+    point_idx: int
+    latitude: float
+    longitude: float
+    timestamp: str
+    meter_reading: float
+    meter_increment: float
+    ride_status: str
+    passenger_count: int
+
+# Second we let Beam know about our Schema by registering it
+beam.coders.registry.register_coder(TaxiPoint, beam.coders.RowCoder)
+
+########################################################################################
+# TASK 1 : A DoFn that converts from python dict to a Beam Schema
+########################################################################################
+class CreateTaxiPoint(beam.DoFn):
+
+    def process(self, element):
+        yield TaxiPoint(**element)
+
+########################################################################################
+# TASK 2 : Write a DoFn "AddTimestampDoFn"
+########################################################################################
+class AddTimestampDoFn(beam.DoFn):
+
+   def process(self, element):
+       # TODO - complete DoFn
+       element_timestamp: float = parser.parse(element.timestamp).timestamp()
+       yield TimestampedValue(element, element_timestamp)
+
+########################################################################################
+#  TASK 3 : Write a PTransform "AddKeysToTaxiRides"
+########################################################################################
+
+class AddKeysToTaxiRides(beam.PTransform):
+
+    def expand(self, pcoll):
+        # identify unique keys per Taxipoint
+        # TODO - complete DoFn
+        keys: PCollection[tuple(str, TaxiPoint)] = pcoll | "addkeys" >> beam.WithKeys(lambda x: x.ride_id)
+        # keys: PCollection[tuple(str, TaxiPoint)] = pcoll | "addkeys" >> beam.Map(lambda x: (x.ride_id, x))
+        return keys
+
+########################################################################################
+#  TASK 4 : Write a PTransform "TaxiSessioning"
+########################################################################################
+
+class TaxiSessioning(beam.PTransform):
+
+    def expand(self, pcoll):
+        # TODO complete PTransform
+        windowed: PCollection[tuple(str, TaxiPoint)] = pcoll | "sessioning" >> beam.WindowInto(
+            windowfn=Sessions(480.0), #8mins --> Where in event time to calculate ?
+            trigger=AfterWatermark(), #when in processing to emit results
+            accumulation_mode=AccumulationMode.DISCARDING,  # how to do refinement of results relate?
+            allowed_lateness=Duration(seconds=0),
+        )
+        return windowed
+
+#######################################################################################
+# TASK 5 : Write a DoFn "BusinessRulesDoFn" for taxi ride statistics
+#######################################################################################
+
+class BusinessRulesDoFn(beam.DoFn):
+    """This DoFn applies some business rules to a group of messages in a window.
+        It leverages the grouping by session done thanks to the Window applied
+        in the pipeline.
+        Here, we will be only handling messages that belong to the same session,
+        identified by ride_id.
+        """
+
+    def __init__(self):
+        # Create metrics to count occurernce of timestamp types
+        # These are not visible with DirectRunner only DataflowRunner
+        # See these docs for details
+        # https://beam.apache.org/documentation/programming-guide/#metrics
+        self.correct_timestamps = beam.metrics.Metrics.counter(
+            self.__class__,
+            'correct_timestamps')
+        self.wrong_timestamps = beam.metrics.Metrics.counter(
+            self.__class__,
+            'wrong_timestamps')
+
+    def _parse_timestamp(self, s):
+        # Default value in case of error
+        d = datetime(1979, 2, 4, 0, 0, 0)
+        try:
+            d = parser.parse(s)
+            self.correct_timestamps.inc()
+        except ValueError:
+            self.wrong_timestamps.inc()
+        return d
+
+    def taxi_point_analyzer(self, points: list[TaxiPoint]) -> dict:
+        info = {'min_timestamp' : None,
+                'max_timestamp' : None,
+                'init_status' : "NA",
+                'end_status' : "NA",
+                'count' : 0,
+                'duration': 0}
+        # Find start time & status. Find end time & status. Calculate stats.
+        # TODO - complete method
+        for taxi_point in points:
+            event_timestamp = self._parse_timestamp(taxi_point.timestamp)
+            ride_status = taxi_point.ride_status
+            if not info['min_timestamp']:
+                info['min_timestamp'] = event_timestamp
+            if not info['max_timestamp']:
+                info['max_timestamp'] = event_timestamp
+            if event_timestamp <= info['min_timestamp']:
+                info['min_timestamp'] = event_timestamp
+                info['init_status'] = ride_status
+            if event_timestamp >= info['max_timestamp']:
+                info['max_timestamp'] = event_timestamp
+                info['end_status'] = ride_status
+            info['count'] += 1
+        info['duration'] = (info['max_timestamp'] - info['min_timestamp']).total_seconds()
+        info['min_timestamp'] = info['min_timestamp'].isoformat()
+        info['max_timestamp'] = info['max_timestamp'].isoformat()
+        return info
+
+
+    def window_analzyer(self, window: beam.DoFn.WindowParam) -> dict:
+        return   {
+            'window_start': window.start.to_rfc3339(),  # rfc3339 is an iso formater
+            'window_end': window.end.to_rfc3339()
+        }
+
+    def pane_analzyer(self, pane_info: beam.DoFn.PaneInfoParam) -> dict:
+        # A pane is the aggregated results of each window.
+        # In pipeline step "sessions" the session window was given a trigger config.
+        # Beam uses this trigger to determine when to emit/fire panes.
+        # We use the pane_info additional param in our DoFn to inspect pane firing.
+        timing = "N/A"
+        # TODO - complete method
+        if pane_info.timing == 0:
+            timing = "EARLY"
+        elif pane_info.timing == 1:
+            timing = "ON_TIME"
+        elif pane_info.timing == 2:
+            timing = "LATE"
+        else:
+            timing = "UNKNOWN"
+        return {'trigger': timing}  # early, on time (watermark) or late
+
+    def process(self,
+                element,
+                window=beam.DoFn.WindowParam,
+                pane_info=beam.DoFn.PaneInfoParam):
+        # element is a tuple ==> PCollection[tuple(str,list(TaxiPoint))]
+        ride_id, list_of_taxi_point = element
+        rideinfo: dict = {'ride_id': ride_id,}
+
+        pointinfo: dict  = self.taxi_point_analyzer(list_of_taxi_point)
+
+        windowinfo: dict = self.window_analzyer(window)
+
+        paneinfo: dict = self.pane_analzyer(pane_info)
+
+        yield rideinfo | pointinfo | windowinfo | paneinfo
+
+########################################################################################
+# TASK 6 : PTransform to represent the core pipeline logic (excludes input + output)
+########################################################################################
+
+class TaxiStatsTransform(beam.PTransform):
+
+    def expand(self, pcoll):
+
+        ridedata: PCollection[dict] = pcoll |"parse json strings">> beam.Map(json.loads)
+        schema: PCollection[TaxiPoint] = ridedata | "apply schemas" >> beam.ParDo(
+            CreateTaxiPoint()).with_output_types(TaxiPoint)
+        tstamp: PCollection[TaxiPoint] = schema | "timestamping" >> beam.ParDo(
+            AddTimestampDoFn())
+        key: PCollection[tuple(str,TaxiPoint)] = tstamp | "key" >> AddKeysToTaxiRides()
+        win: PCollection[tuple(str,TaxiPoint)] = key | "sessions" >> TaxiSessioning()
+        grp: PCollection[tuple(str,list(TaxiPoint))] = win | "group">> beam.GroupByKey()
+        stats: PCollection[dict] = grp | "stats" >> beam.ParDo(BusinessRulesDoFn())
+        return stats
+
+#######################################################################################
+# Pipeline
+#######################################################################################
+
+def run(
+    beam_options: Optional[PipelineOptions] = None
+) -> None:
+
+    with beam.Pipeline(options=beam_options) as pipeline:
+        rides: PCollection[str] = pipeline | "Read ndjson input" >> ReadFromText(
+            file_pattern=beam_options.input_filename)
+        calculations: PCollection[dict] = rides | "calculations" >> TaxiStatsTransform()
+        writeablecalculations: PCollection[str] = calculations | beam.Map(json.dumps)
+        writeablecalculations | WriteToFiles(path=beam_options.output_filename)
